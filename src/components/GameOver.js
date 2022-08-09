9a6919592a6ebde9bee4db2d06f1f8c009b81e19
Rahul Singh <rahulapril20@gmail.com>
2022-08-09 23:15:12 +0530

    updated code

---
+++ src/components/GameOver.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../App';
+
+function GameOver() {
+    const {gameOver, currAttempt, correctWord} = useContext(AppContext);
+  return (
+    <div className='gameOver'>
+        <h3>{gameOver.guessedWord ? "You correctly guessed" : "You failed"}</h3>
+        <h1>Correct : {correctWord} </h1>
+        {gameOver.guessedWord ? (<h3>You guessed in {currAttempt.attempt} attempts</h3>) : (<h3> Better luck next time </h3>)}
+    </div>
+  )
+}
+
+export default GameOver
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,10 +1,29 @@
-import React, {useContext} from 'react';
-import {AppContext} from '../App'
+import React, { useContext, useEffect } from "react";
+import { AppContext } from "../App";
 
 function Letter({ letterPos, attemptVal }) {
-  const {board} = useContext(AppContext);
-    const letter = board[attemptVal][letterPos];
-  return <div className='letter'>{letter}</div>;
+  const { board, correctWord, currAttempt, setDisabledLetter } = useContext(AppContext);
+  const letter = board[attemptVal][letterPos];
+
+  const correct = correctWord.toUpperCase()[letterPos] === letter;
+  const almost = !correct && letter !== "" && correctWord.includes(letter);
+
+  const letterState = 
+    currAttempt.attempt > attemptVal &&
+    (correct ? "correct" : almost ? "almost" : "error");
+
+    useEffect(() => {
+      if(letter != "" && !correct && !almost) {
+        setDisabledLetter((prev) => [...prev, letter]);
+      }
+    }, [currAttempt.attempt]);
+
+  return (
+    <div className="letter" id={letterState}>
+      {" "}
+      {letter}
+    </div>
+  );
 }
 
 export default Letter;
